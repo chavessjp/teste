4641427a98def3bb552f0f421fa607412dd1e2aa
chavessjp <joaochaves@outlook.com>
2022-04-24 00:09:33 -0300

    segundo commit

--- src/projetogit/ProjetoGit.java
+++ src/projetogit/ProjetoGit.java
@@ -14,7 +14,10 @@ public class ProjetoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int i = 20;
+        
+        
     }
     
 }
